12cb9b16cafbfeb03e6cb62a268ff618fa5b5a06
KyleTruong <ktruong008@gmail.com>
2023-03-08 13:24:08 -0500

    Better message formatting

--- src/lib/modules/chat/chat-message.svelte
+++ src/lib/modules/chat/chat-message.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
-	export let role = '';
-	export let message = '';
+  export let role = '';
+  export let message = '';
 
-	const imgSrc = role === 'assistant' ? '/alfred-headshot.png' : '/batman-headshot.png';
-	const name = role === 'assistant' ? 'Alfred' : 'Batman';
+  const imgSrc = role === 'assistant' ? '/alfred-headshot.png' : '/batman-headshot.png';
+  const name = role === 'assistant' ? 'Alfred' : 'Batman';
 </script>
 
 <li class="flex py-4">
-	<img class="h-10 w-10 rounded-full" src={imgSrc} alt="" />
-	<div class="ml-3">
-		<p class="text-sm font-medium text-gray-900">{name}</p>
-		<p class="text-sm text-gray-500">{message}</p>
-	</div>
+  <img
+    class="h-10 w-10 rounded-full"
+    src={imgSrc}
+    alt=""
+  />
+  <div class="ml-3">
+    <p class="text-sm font-medium text-gray-900">{name}</p>
+    <p class="text-sm text-gray-500 whitespace-pre-line">
+      {message}
+    </p>
+  </div>
 </li>

8a1d7b118baef7cfd431860896d8f86c231a99b7
KyleTruong <ktruong008@gmail.com>
2023-03-09 15:25:09 -0500

    mobile navbar ui boilerplate

--- src/lib/modules/sidebar/mobile-hamburger.svelte
+++ src/lib/modules/sidebar/mobile-hamburger.svelte
@@ -1,11 +1,12 @@
 <script>
+  import PlusIcon from '$lib/shared/icons/plus-icon.svelte';
   import { getContext } from 'svelte';
 
   let { openMobileSidebar } = getContext('sidebar');
 </script>
 
 <div
-  class="sticky flex justify-end top-0 z-10 md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3 bg-gray-800"
+  class="sticky flex justify-between items-center top-0 z-10 md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3 bg-gray-800"
 >
   <button
     type="button"
@@ -29,5 +30,16 @@
         d="M4 6h16M4 12h16M4 18h16"
       />
     </svg>
+  </button>
+
+  <span class="text-gray-300"> Hello </span>
+
+  <button
+    type="button"
+    class="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+    on:click={openMobileSidebar}
+  >
+    <span class="sr-only">New chat</span>
+    <PlusIcon />
   </button>
 </div>

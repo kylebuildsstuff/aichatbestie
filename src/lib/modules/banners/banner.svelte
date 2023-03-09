6662285c1f0e6480802ab0a6c5cff8b2f4f93f28
KyleTruong <ktruong008@gmail.com>
2023-03-09 18:17:57 -0500

    Update banner icons

--- src/lib/modules/banners/banner.svelte
+++ src/lib/modules/banners/banner.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import XCircleIcon from '$lib/shared/icons/x-circle-icon.svelte';
+  import XMarkIcon from '$lib/shared/icons/x-mark-icon.svelte';
   import { banners$ } from '$lib/shared/shared.store';
 
   export let bannerId = '';
@@ -15,20 +17,7 @@
 <div class="rounded-md bg-red-50 p-4 my-2">
   <div class="flex">
     <div class="flex-shrink-0">
-      <!-- Heroicon name: solid/x-circle -->
-      <svg
-        class="h-5 w-5 text-red-400"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        aria-hidden="true"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
-          clip-rule="evenodd"
-        />
-      </svg>
+      <XCircleIcon />
     </div>
     <div class="ml-3">
       {#if title}
@@ -52,20 +41,7 @@
           class="inline-flex bg-red-50 rounded-md p-1.5 text-red-400 hover:bg-red-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-red-50 focus:ring-red-600"
         >
           <span class="sr-only">Dismiss</span>
-          <!-- Heroicon name: solid/x -->
-          <svg
-            class="h-5 w-5"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            />
-          </svg>
+          <XMarkIcon />
         </button>
       </div>
     </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+
   import PageContainer from '$lib/modules/page-container/page-container.svelte';
   import Hero from '$lib/shared/components/hero.svelte';
   import Chat from '$lib/modules/chat/chat.svelte';
+  import { banners$ } from '$lib/shared/shared.store';
+
+  onMount(() => {
+    banners$.update((state) => [
+      ...state.filter((banner) => banner.bannerId !== 'sss'),
+      {
+        bannerId: 'sss',
+        title: 'A problem occurred while changing plans',
+        description: 'Please refresh the page and try again later, or contact support.'
+      }
+    ]);
+  });
 </script>
 
 <PageContainer>

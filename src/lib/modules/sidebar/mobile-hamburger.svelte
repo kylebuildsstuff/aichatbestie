f2e36bd669148068547d0709cb75a82112e81d92
KyleTruong <ktruong008@gmail.com>
2023-03-09 15:41:35 -0500

    Add new chat button to mobile nav

--- src/lib/modules/sidebar/mobile-hamburger.svelte
+++ src/lib/modules/sidebar/mobile-hamburger.svelte
@@ -1,22 +1,46 @@
 <script>
   import { getContext } from 'svelte';
+  import { nanoid } from 'nanoid';
   import { page as page$ } from '$app/stores';
 
   import { chatList$, chats$ } from '$lib/shared/shared.store';
   import MenuIcon from '$lib/shared/icons/menu-icon.svelte';
   import PlusIcon from '$lib/shared/icons/plus-icon.svelte';
-  import { truncateString } from '$lib/shared/shared-utils';
+  import {
+    createNewChat,
+    createNewChatListItem,
+    truncateString
+  } from '$lib/shared/shared-utils';
+  import { LOCAL_STORAGE_KEY } from '$lib/shared/shared.type';
+  import { goto } from '$app/navigation';
 
-  let { openMobileSidebar } = getContext('sidebar');
+  let { openMobileSidebar, handleCloseMobileSidebar } = getContext('sidebar');
 
   $: chatId = $page$.params.chatId;
-  // $: title = $chats$[chatId]?.title;
   $: title = truncateString(
     $chatList$.find((chat) => chat.chatId === chatId)?.title || 'New chat',
     40
   );
 
-  // get page params
+  const handleCreateNewChat = () => {
+    // https://zelark.github.io/nano-id-cc/
+    const newChatId = nanoid(5);
+
+    chatList$.update((chatList) => {
+      chatList.unshift(createNewChatListItem(newChatId));
+      return chatList;
+    });
+    chats$.update((chats) => {
+      chats[newChatId] = createNewChat(newChatId);
+      return chats;
+    });
+
+    localStorage.setItem(LOCAL_STORAGE_KEY.CHAT_LIST, JSON.stringify($chatList$));
+    localStorage.setItem(newChatId, JSON.stringify($chats$[newChatId]));
+
+    goto(`/chat/${newChatId}`);
+    handleCloseMobileSidebar();
+  };
 </script>
 
 <div
@@ -36,7 +60,7 @@
   <button
     type="button"
     class="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-    on:click={openMobileSidebar}
+    on:click={handleCreateNewChat}
   >
     <span class="sr-only">New chat</span>
     <PlusIcon />

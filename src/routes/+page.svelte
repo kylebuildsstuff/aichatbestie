9ae27c6cbe6d65dc0513840aee340b7d4bee9ed0
KyleTruong <ktruong008@gmail.com>
2023-03-09 18:49:53 -0500

    Clear out banner

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,7 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-
   import PageContainer from '$lib/modules/page-container/page-container.svelte';
   import Hero from '$lib/shared/components/hero.svelte';
   import Chat from '$lib/modules/chat/chat.svelte';
-  import { banners$ } from '$lib/shared/shared.store';
-  import { ERROR } from '$lib/shared/shared.type';
-
-  onMount(() => {
-    banners$.update((state) => [
-      ...state.filter(
-        (banner) => banner.bannerId !== ERROR.LOCAL_STORAGE_STORAGE_LIMIT_REACHED
-      ),
-      {
-        bannerId: ERROR.LOCAL_STORAGE_STORAGE_LIMIT_REACHED,
-        title: 'Browser storage running low',
-        description:
-          'Old chats will be overwritten, please delete unused chats to free up space.'
-      }
-    ]);
-  });
 </script>
 
 <PageContainer>

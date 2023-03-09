e386872424b0fa6dd464211e8fd3a1e10937b9e9
KyleTruong <ktruong008@gmail.com>
2023-03-09 12:06:27 -0500

    clean chat message

--- src/lib/modules/chat/chat-message.svelte
+++ src/lib/modules/chat/chat-message.svelte
@@ -5,7 +5,6 @@
   export let content = '';
 
   const imgSrc = role === 'assistant' ? '/alfred-headshot.png' : '/batman-headshot.png';
-  const name = role === 'assistant' ? 'Alfred' : 'Batman';
 </script>
 
 <li class="w-full mx-auto flex py-4 max-w-md lg:max-w-2xl xl:max-w-4xl">
@@ -16,35 +15,11 @@
   />
   <div class="ml-3">
     <p class="prose max-w-max text-sm text-gray-500 w-full">
-      <!-- {@html marked.parse(content)} -->
       {@html marked.parse(content)}
     </p>
   </div>
 </li>
 
-<!-- <script lang="ts">
-  import { marked } from 'marked';
-
-  export let role = '';
-  export let content = '';
-
-  const imgSrc = role === 'assistant' ? '/alfred-headshot.png' : '/batman-headshot.png';
-  const name = role === 'assistant' ? 'Alfred' : 'Batman';
-</script>
-
-<li class="w-full mx-auto flex py-4 max-w-md lg:max-w-2xl xl:max-w-4xl">
-  <img
-    class="h-10 w-10 rounded-full"
-    src={imgSrc}
-    alt=""
-  />
-  <div class="ml-3">
-    <p class="text-sm font-medium text-gray-900">{name}</p>
-    <p class="prose text-sm text-gray-500 whitespace-pre-line">
-      {@html marked.parse(content)}
-    </p>
-  </div>
-</li> -->
 <style>
   :global(pre) {
     white-space: pre-wrap;

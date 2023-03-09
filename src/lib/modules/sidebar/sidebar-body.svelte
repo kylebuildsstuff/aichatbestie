6918bffba5a38c8719833073beebf561d6a85bd2
KyleTruong <ktruong008@gmail.com>
2023-03-09 17:43:01 -0500

    Add search UI

--- src/lib/modules/sidebar/sidebar-body.svelte
+++ src/lib/modules/sidebar/sidebar-body.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { nanoid } from 'nanoid';
   import { getContext } from 'svelte';
+  import Fuse from 'fuse.js';
   import { goto } from '$app/navigation';
 
   import PlusIcon from '$lib/shared/icons/plus-icon.svelte';
@@ -9,8 +10,26 @@
   import { LOCAL_STORAGE_KEY } from '$lib/shared/shared.type';
 
   import SidebarChatItem from './sidebar-chat-item.svelte';
+  import MagnifyingGlassIcon from '$lib/shared/icons/magnifying-glass-icon.svelte';
 
   let { handleCloseMobileSidebar } = getContext('sidebar') as any;
+
+  const chatListFuseOptions = {
+    // Lower threshold = closer match
+    threshold: 0.4,
+    keys: ['title']
+  };
+
+  const chatsFuseOptions = {
+    // Lower threshold = closer match
+    threshold: 0.7,
+    keys: ['messages.content']
+  };
+
+  let searchInput = '';
+
+  $: chatListFuse = new Fuse($chatList$, chatListFuseOptions);
+  $: chatsFuse = new Fuse(Object.values($chats$), chatsFuseOptions);
 
   const handleCreateNewChat = () => {
     // https://zelark.github.io/nano-id-cc/
@@ -35,15 +54,36 @@
 
 <div class="flex flex-1 flex-col overflow-auto">
   <nav class="flex-1 px-2 py-3 bg-gray-800 space-y-1">
+    <!-- New chat -->
     <button
       on:click={handleCreateNewChat}
-      class={`w-full text-gray-300 hover:bg-gray-700 flex items-center px-2 py-3 text-sm font-medium rounded-md mb-3 shadow-sm ring-1 ring-inset ring-gray-400 `}
+      class={`w-full text-gray-300 hover:bg-gray-500 flex items-center px-2 py-3 text-sm font-medium rounded-md mb-3 shadow-sm ring-1 ring-inset ring-gray-400 `}
     >
       <PlusIcon
         overrideClasses={'text-gray-400 hover:text-gray-500 mr-3 flex-shrink-0 h-5 w-5'}
       />
       New chat
     </button>
+
+    <!-- Search -->
+    <div class="relative flex flex-grow items-stretch focus-within">
+      <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+        <MagnifyingGlassIcon />
+      </div>
+      <input
+        bind:value={searchInput}
+        placeholder="Search"
+        type="text"
+        name="search"
+        class="block w-full rounded-none rounded-l-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      />
+      <div class="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
+        <kbd
+          class="inline-flex items-center rounded border border-gray-200 px-1 font-sans text-xs text-gray-400"
+          >⌘K</kbd
+        >
+      </div>
+    </div>
 
     {#each $chatList$ as { chatId: cId, title }, index}
       <SidebarChatItem

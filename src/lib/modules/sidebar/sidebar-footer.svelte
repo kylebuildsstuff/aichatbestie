4698c743f18860da948abf36194f02352dea3013
KyleTruong <ktruong008@gmail.com>
2023-03-09 12:48:48 -0500

    Add more footer stuff

--- src/lib/modules/sidebar/sidebar-footer.svelte
+++ src/lib/modules/sidebar/sidebar-footer.svelte
@@ -7,6 +7,7 @@
 </script>
 
 <div class="flex flex-col border-t border-gray-200">
+  <!-- Upgrade -->
   <a
     on:click={handleCloseMobileSidebar}
     href="/settings"
@@ -17,23 +18,41 @@
 
       <div class="flex items-center">
         <p class={`text-base font-medium text-gray-700 group-hover:text-gray-900`}>
-          Settings
+          Upgrade
         </p>
       </div>
     </div>
   </a>
 
+  <!-- Join -->
   <a
     on:click={handleCloseMobileSidebar}
     href="/settings"
     class="w-full p-4 hover:bg-gray-50"
   >
-    <div class="flex items-center justify-start">
+    <div class="flex items-center gap-2 justify-start">
       <CogIcon />
 
       <div class="flex items-center">
         <p class={`text-base font-medium text-gray-700 group-hover:text-gray-900`}>
-          Settings
+          Change API key
+        </p>
+      </div>
+    </div>
+  </a>
+
+  <!-- Change API key -->
+  <a
+    on:click={handleCloseMobileSidebar}
+    href="/settings"
+    class="w-full p-4 hover:bg-gray-50"
+  >
+    <div class="flex items-center gap-2 justify-start">
+      <CogIcon />
+
+      <div class="flex items-center">
+        <p class={`text-base font-medium text-gray-700 group-hover:text-gray-900`}>
+          Join <span class="text-gray-400 text-sm">(optional)</span>
         </p>
       </div>
     </div>

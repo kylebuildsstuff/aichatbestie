97e3ed7fd4d5689d3bd0335c82468f6326ead93f
KyleTruong <ktruong008@gmail.com>
2023-03-09 10:20:09 -0500

    Add marked, format some stuff

--- src/lib/modules/chat/chat-message.svelte
+++ src/lib/modules/chat/chat-message.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { marked } from 'marked';
+
   export let role = '';
   export let content = '';
 
@@ -14,8 +16,32 @@
   />
   <div class="ml-3">
     <p class="text-sm font-medium text-gray-900">{name}</p>
-    <p class="text-sm text-gray-500 whitespace-pre-line">
-      {content}
+    <p class="prose text-sm text-gray-500 max-w-md lg:max-w-2xl xl:max-w-4xl">
+      {@html marked.parse(content)}
     </p>
   </div>
 </li>
+
+<!-- <script lang="ts">
+  import { marked } from 'marked';
+
+  export let role = '';
+  export let content = '';
+
+  const imgSrc = role === 'assistant' ? '/alfred-headshot.png' : '/batman-headshot.png';
+  const name = role === 'assistant' ? 'Alfred' : 'Batman';
+</script>
+
+<li class="w-full mx-auto flex py-4 max-w-md lg:max-w-2xl xl:max-w-4xl">
+  <img
+    class="h-10 w-10 rounded-full"
+    src={imgSrc}
+    alt=""
+  />
+  <div class="ml-3">
+    <p class="text-sm font-medium text-gray-900">{name}</p>
+    <p class="prose text-sm text-gray-500 whitespace-pre-line">
+      {@html marked.parse(content)}
+    </p>
+  </div>
+</li> -->

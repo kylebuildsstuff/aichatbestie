1b5b1743984edeb810fe43230888133f3ffed116
KyleTruong <ktruong008@gmail.com>
2023-03-14 14:52:29 -0400

    Prep netlify build

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */

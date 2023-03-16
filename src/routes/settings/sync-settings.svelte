d76aae7a3903a911a96a1460b297d8ed51631af0
KyleTruong <ktruong008@gmail.com>
2023-03-16 10:11:30 -0400

    More UI work on data sync settings

--- src/routes/settings/sync-settings.svelte
+++ src/routes/settings/sync-settings.svelte
@@ -44,18 +44,35 @@
 
 <div class="divide-y divide-gray-200">
   <!-- Section header -->
-  <div class="flex items-center justify-between space-y-1">
+  <div class="flex flex-col justify-start space-y-1">
     <h3 class="text-lg leading-6 font-medium text-gray-900">Data syncing</h3>
+    <p class="max-w-2xl text-sm text-gray-500">
+      Importing saved chats will overwrite your current chat history.
+    </p>
   </div>
 
   <div class="mt-6">
     <!-- Password -->
     <dl class="divide-y divide-gray-200">
-      <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 items-center">
+      <div class="flex items-center justify-between py-4 sm:py-5">
         <dt class="text-sm font-medium text-gray-500">Chat history</dt>
 
-        <dd class="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-          <span class="flex-grow">**********</span>
+        <dd class="flex gap-2 text-sm text-gray-900">
+          <button
+            type="button"
+            class="inline-flex items-center bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            on:click={handleEditClick}
+          >
+            Save
+          </button>
+
+          <button
+            type="button"
+            class="inline-flex items-center py-2 px-3 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            on:click={handleEditClick}
+          >
+            Import
+          </button>
         </dd>
       </div>
     </dl>
